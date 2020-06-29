0b417b116fc5fc0dc3d9b328d189cdcd5c7a22ce
Alex Biehl <32490273+ab7289@users.noreply.github.com>
2020-06-29 14:10:18 -0400

    added more spell tests

--- tests/test_spell.py
+++ tests/test_spell.py
@@ -12,3 +12,19 @@ def test_index(client, auth):
     assert b"Log Out" in response.data
     assert b"Spellr" in response.data
     assert b"test" in response.data
+
+
+def test_spell_valid(client, auth):
+    auth.login()
+    assert client.get("/spell_check").status_code == 200
+    response = client.post("/spell_check", data={"inputtext": "This is a test."})
+
+    assert b"All words are correctly spelled" in response.data
+
+
+def test_spell_invalid(client, auth):
+    auth.login()
+    assert client.get("/spell_check").status_code == 200
+    response = client.post("/spell_check", data={"inputtext": "This is a tset."})
+
+    assert b"words are misspelled" in response.data
