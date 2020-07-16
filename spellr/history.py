3154033ea71f1e9ec2e3a065711f374a0bf2aeb5
Alex Biehl <32490273+ab7289@users.noreply.github.com>
2020-07-16 08:34:22 -0400

    added item template and routing function

--- spellr/history.py
+++ spellr/history.py
@@ -6,7 +6,7 @@ from flask import (
 from flask_login import login_required
 
 from spellr.forms import HistoryForm
-from spellr.models import User  # , Question
+from spellr.models import User, Question
 from spellr.util import flash_errors
 
 bp = Blueprint("history", __name__)
@@ -43,4 +43,7 @@ def history():
 @bp.route("/history/query<int:item_id>", methods=("GET",))
 @login_required
 def item(item_id):
-    return "Hello, world!"
+
+    item = Question.query.get(item_id)
+
+    return render_template("history/item.html", item_id=item_id, item=item)
