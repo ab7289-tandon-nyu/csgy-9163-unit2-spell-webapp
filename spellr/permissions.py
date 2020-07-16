4c68e7d89fc5b8aab7c38e4ea2b8288181afe260
Alex Biehl <32490273+ab7289@users.noreply.github.com>
2020-07-15 20:11:45 -0400

    started adding specific permissions, will build out once posts are done

---
+++ spellr/permissions.py
@@ -0,0 +1,9 @@
+# from flask_principal import RoleNeed, Permission
+# from collections import namedtuple
+# from functools import partial
+
+# flask-login needs
+# user_need = RoleNeed("user")
+# admin_need = RoleNeed("admin")
+
+# see_history = namedtuple()
