14895e4b9c77f4ecd56d7d4ed71bcb204c91b35f
Alex Biehl <32490273+ab7289@users.noreply.github.com>
2020-07-18 17:41:59 -0400

    added tests

--- tests/test_history.py
+++ tests/test_history.py
@@ -56,3 +56,29 @@ def test_history_item_found(auth, client):
     assert b"no errors" in response.data
     assert b"test" in response.data
     assert f"Query{q.id}" in str(response.data)
+
+
+def test_login_history_auth(auth, client):
+    assert client.get("/login_history").status_code == 302
+    auth.login()
+    assert client.get("/login_history").status_code == 200
+
+
+@pytest.mark.parametrize(
+    ("query", "message"),
+    (
+        ("a", b"Field must be between 3 and 25 characters long"),
+        (
+            "asdfasdfasdfasdfasdfasdfasdf",
+            b"Field must be between 3 and 25 characters long",
+        ),
+        ("test2", b"Unable to find a registered user for user_id"),
+        ("test", b"Login ID"),
+    ),
+)
+def test_login_history_results(auth, client, query, message):
+    auth.login()
+    response = client.post("/login_history", data={"userid": query})
+
+    assert response.status_code == 200
+    assert message in response.data
--- tests/test_spell.py
+++ tests/test_spell.py
@@ -3,7 +3,6 @@ from spellr.models import User
 
 def test_index(client, auth):
     response = client.get("/spell_check")
-    print(f"result1: {response}")
     assert response.status_code == 302
     assert "http://localhost/auth/login" == response.headers["Location"]
 
